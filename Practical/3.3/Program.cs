e09e9201e4df5efc08b7384b0a36a7c4f5d47c43
mycubicle13 <mycubicle13@gmail.com>
2018-04-15 11:11:32 +0900

    3.3.2 Find Method

--- Practical/3.3/Program.cs
+++ Practical/3.3/Program.cs
@@ -10,7 +10,42 @@ namespace _3._3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello GitHub!!");
+            var list = new List<string>
+            {
+                "Seoul", "New Delhi", "bangkok", "London", "Paris", "Berlin", "Canberra", "Hong Kong",
+            };
+
+
+            // 3.3.2 Find 메서드
+
+            string name = list.Find(s => s.Length == 6);
+            Console.WriteLine(name);
+
+            string answer = Find(list, s => s.Length == 6);
+            Console.WriteLine(answer);
+            
+
+
+        }
+
+
+        public static string Find(List<string> list, Predicate<string> judge)
+        {
+            string result = string.Empty;
+
+            foreach (var item in list)
+            {
+                if (judge(item))
+                {
+                    result = item;
+                    break;
+                }
+
+            }
+
+
+            return result;
+
         }
     }
 }
