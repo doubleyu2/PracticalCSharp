4612b4e7872078ec447a9d5c5890077b2ba4c864
mycubicle13 <mycubicle13@gmail.com>
2018-04-19 10:05:27 +0900

    Column Query 문

--- Practical/3.3/Program.cs
+++ Practical/3.3/Program.cs
@@ -16,37 +16,19 @@ namespace _3._3
             };
 
 
-            // 3.4.4
+            var query = from s in list
+                        where s.Length >= 5
+                        select s.ToUpper();
 
-            // where 메서드만 쓰면 지연실행 상태가 됨.
-            // ToList나 ToArray 같이 바꿔주는 메서드를 실행하면 즉시 결과가 저장됨.
-            //IEnumerable<string> answer = list.Where(s => s.Length <= 5
-            IEnumerable<string> answer = list.Where(s => s.Length <= 5).ToList();
-
-
-            foreach (var item in answer)
+            foreach (string s in query)
             {
-                Console.WriteLine(item);
-            }
-
-            list[0] = "Busan";
-
-            foreach (var item in answer)
-            {
-                Console.WriteLine(item);
-            }
-
-            list[0] = "Busap";
-
-            foreach (var item in answer)
-            {
-                Console.WriteLine(item);
+                Console.WriteLine(s);
             }
 
 
-            var count = list.Count(s => s.Length <= 5);
-            Console.WriteLine(count);
+            var query2 = list.Where(s => s.Length >= 5).Select(s => s.ToUpper()).ToList();
 
+            query2.ForEach(s => Console.WriteLine(s));
 
 
         }
