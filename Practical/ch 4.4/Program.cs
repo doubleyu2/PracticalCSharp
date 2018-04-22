76241c19baba68b1ffffabb53d8b623ea9661043
mycubicle13 <mycubicle13@gmail.com>
2018-04-22 12:04:58 +0900

    4.6.2 옵션 인수

--- Practical/ch 4.4/Program.cs
+++ Practical/ch 4.4/Program.cs
@@ -10,31 +10,18 @@ namespace ch_4._4
     {
         static void Main(string[] args)
         {
-            var obj = new MySample();
-            obj.MyList.Add(6);              // obj 의 MyList 에 add. Get 만을 사용 
-
-            obj.Print();
-
-            obj.MyList = new List<int>();
+            Print("안녕하세요");
 
         }
 
-
+        public static void Print(string msg, int n=1)
+        {
+            for(int i = 1; i<= n; i++)
+            {
+                Console.WriteLine(msg);
+            }
+        }
 
     }
     
-    class MySample
-    {
-        public IReadOnlyList<int> MyList { get; private set; }
-
-        public MySample()
-        {
-            MyList = new List<int>() { 1, 2, 3, 4, 5, };
-        }
-
-        public void Print()
-        {
-            MyList.ForEach(i => Console.WriteLine(i));
-        }
-    }
 }
