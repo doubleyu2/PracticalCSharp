ff00de7a788d9f8fef567a2490a31da34c0e8087
mycubicle13 <mycubicle13@gmail.com>
2018-04-22 10:10:29 +0900

    문제 3.1.1 정답

---
+++ Practical/3장 연습문제 정답/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3장_연습문제_정답
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new List<int>
+            {
+                12, 87, 94,14,53,20,40,35,76,91,31,17,48
+            };
+
+            // 문제 3.1.1
+
+            if (numbers.Exists(i => i % 8 == 0 || i % 9 == 0))
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine("false");
+            }
+
+        }
+    }
+}
