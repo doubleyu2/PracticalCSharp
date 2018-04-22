f2b5f2b099136718727e89d0a0aaabfd60c704d0
mycubicle13 <mycubicle13@gmail.com>
2018-04-22 11:48:20 +0900

    4.5.2 읽기 전용 람다식

--- Practical/ch 4.4/Program.cs
+++ Practical/ch 4.4/Program.cs
@@ -22,13 +22,13 @@ namespace ch_4._4
         // 읽기 전용. 클래스 내부에서만 접근 가능
         public int Length { get; private set; } = 5;
 
-        // 읽기 전용. set이 없으면 생성자에서 지정이 가능하다. 또는 직접 초기화
-        public string Name { get; } = "aaa";
+        // 읽기 전용. 고정
+        public string Name => "홍길동";
 
         public Sale()     // 생성자 method
         {
             Length = 5;
-            Name = "홍길동";
+           
         }
     
         
