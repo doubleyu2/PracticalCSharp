2895243f292370e2a1c75826fcdcde01bf3ae132
mycubicle13 <mycubicle13@gmail.com>
2018-04-22 11:17:50 +0900

    4.3.3 List<t>의 Foreach

---
+++ Practical/chap 4.3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chap_4._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nums = new List<int> { 1, 2, 3, 4, 5 };
+            nums.ForEach(n => Console.WriteLine(n));
+
+        }
+    }
+}
