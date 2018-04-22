ba498a6b5ac9971d03fd99d6eeb4275170d61683
mycubicle13 <mycubicle13@gmail.com>
2018-04-22 11:28:38 +0900

    ch 4.4

---
+++ Practical/ch 4.4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ch_4._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int intInput = int.Parse(input);
+
+            bool result = intInput % 5 == 0 ? true : false;
+            Console.WriteLine(result);
+        }
+    }
+}
