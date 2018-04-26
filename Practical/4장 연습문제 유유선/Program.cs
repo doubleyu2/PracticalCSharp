6b67dd0df66b00b75da3cef6463c6e9405395a50
mycubicle13 <mycubicle13@gmail.com>
2018-04-26 09:21:31 +0900

    5.2.2 StartsWith 지정한 문자열로 시작하는지 체크

---
+++ Practical/4장 연습문제 유유선/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4장_연습문제_유유선
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class YearMonth
+    {
+        public int Year { get; private set; }
+        public int Month { get; private set; }
+
+        public YearMonth()
+        {
+            Year = 2018, 
+            Month
+        }
+    }
+}
---
+++ Practical/5장/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5장
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Visual Studio";
+
+            if (str.StartsWith("visual", StringComparison.CurrentCultureIgnoreCase))
+            { 
+                Console.WriteLine("시작합니다.");
+            }
+        }
+    }
+}
