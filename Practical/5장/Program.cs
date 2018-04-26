7b8a6268e3b3afb979e407c5c77cfa9292637276
mycubicle13 <mycubicle13@gmail.com>
2018-04-26 10:27:08 +0900

    5.4.2 문자열 Remove 함수

--- Practical/5장/Program.cs
+++ Practical/5장/Program.cs
@@ -10,11 +10,41 @@ namespace _5장
     {
         static void Main(string[] args)
         {
-            string str = "  안녕하세요 ";
+            string str = "012WERJUIJSDF34567";
 
-            string replace = str.Trim();
+            int start = -1;
+            int count = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (!char.IsDigit(str[i]))
+                {
+                    if (start == -1)
+                    {
+                        start = i;
+                    }
+                    count++;
 
-            Console.WriteLine(replace);
+                }
+                else
+                {
+                    if (start != -1)
+                    { 
+                        break;
+                    }
+                }
+            
+
+            }
+
+    
+
+
+    
+            var result = str.Remove(start, count);
+
+            Console.WriteLine(result);
+
+            //숫자만 출력
 
         }
     }
