44a0faa33738ba103527e2d5ba2c6cc75d6bf210
mycubicle13 <mycubicle13@gmail.com>
2018-05-06 10:46:48 +0900

    6장 Average(Func)

--- Practical/6장/Program.cs
+++ Practical/6장/Program.cs
@@ -14,12 +14,32 @@ namespace _6장
         static void Main(string[] args)
         {
 
-            var numbers = new List<int> { 9, 7, 5, 4, 2, 5, 4, 0, 4,1, 0, 4 };
-            var average = numbers.Average();
+            var books = new List<Book>
+            {
+                new Book {Title = "태평천하", Price = 400, Pages = 378 },
+                new Book {Title = "운수 좋은 날", Price = 281, Pages = 212},
+                new Book {Title = "만세전", Price = 389, Pages = 201},
+                new Book {Title = "삼대", Price = 637, Pages = 464},
+                new Book {Title = "상록수", Price = 411, Pages = 276},
+                new Book {Title = "혈의 누", Price = 961, Pages = 666},
+                new Book {Title = "금수회의록", Price = 514, Pages = 268},
+            };
 
-            Console.WriteLine(average);
+            double averPrice = books.Average(b => b.Price);
+            
+            Console.WriteLine(averPrice);
+          
 
 
         }
     }
+
+    class Book
+    {
+        public string Title { get; set; }
+        public int Price { get; set; }
+        public int Pages { get; set; }
+    }
+
+
 }
