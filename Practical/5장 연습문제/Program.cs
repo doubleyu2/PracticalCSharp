fe94735b63dc8cb5a648bcbf39891a7406461830
mycubicle13 <mycubicle13@gmail.com>
2018-05-06 10:19:43 +0900

    6 장 Repeat

---
+++ Practical/5장 연습문제/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5장_연습문제
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 문제 5.1 
+
+            Console.WriteLine("첫번째 문자열을 입력하세요");
+            string str1 = Console.ReadLine();
+
+            Console.WriteLine("두번째 문자열을 입력하세요");
+            string str2 = Console.ReadLine();
+
+            if (str1 != str2)
+            {
+                Console.WriteLine("두개의 문자열이 상이합니다");
+            }
+            else
+            {
+                Console.WriteLine("두개의 문자열이 동일합니다");
+            }
+
+            // 문제 5.2 
+
+        }
+    }
+}
---
+++ Practical/6장/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6장
+{
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var numbers = Enumerable.Repeat(-1, 20).ToList();
+
+            numbers.ForEach(x => Console.WriteLine(x));
+
+
+
+        }
+    }
+}
