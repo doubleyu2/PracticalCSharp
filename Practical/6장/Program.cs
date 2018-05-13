b3e2a452f629577ad70021ba3c6d11233e2fe7ef
mycubicle13 <mycubicle13@gmail.com>
2018-05-13 11:19:23 +0900

    문제 6.2 1-3

--- Practical/6장/Program.cs
+++ Practical/6장/Program.cs
@@ -13,21 +13,39 @@ namespace _6장
     {
         static void Main(string[] args)
         {
-            var numbers = new int[] { 5, 10, 17, 9, 3, 21, 10, 40, 21, 3, 35 };
+            var books = new List<Book> {
+               new Book { Title = "C# 프로그래밍의 상식", Price = 38000, Pages = 378 },
+               new Book { Title = "람다식과 LINQ의 비밀", Price = 25000, Pages = 312 },
+               new Book { Title = "원더풀 C# 라이프", Price = 29000, Pages = 385 },
+               new Book { Title = "독학 병렬처리 프로그래밍", Price = 48000, Pages = 464 },
+               new Book { Title = "구문으로 배우는 C# 입문", Price = 53000, Pages = 604 },
+               new Book { Title = "나도 할 수 있는 ASP.NET MVC", Price = 32000, Pages = 453 },
+               new Book { Title = "재미있는 C# 프로그래밍 교실", Price = 25400, Pages = 348 },
+            };
 
-            var result = numbers.OrderBy(i => i).Take(3).ToList();
+            var temp = books.FirstOrDefault(book => book.Title == "원더풀 C# 라이프");
 
-            result.ForEach(i => Console.WriteLine(i));
+            Console.WriteLine($"Title: {temp.Title}, Price: {temp.Price}, Pages: {temp.Pages}");
 
-            var result2 = numbers.Distinct().Where(i => i > 10).Count();
+            var temp2 = books.Count(book => book.Title.Contains("C#"));
+
+            var temp3 = books.Where(s => s.Title.Contains("C#")).Average(book => book.Pages);
 
             Console.WriteLine();
-            Console.WriteLine(result2);
+            Console.WriteLine(temp2);
 
+            Console.WriteLine();
+            Console.WriteLine(temp3);
 
-        
-
+            
         }
+    }
+
+    class Book
+    {
+        public String Title { get; set; }
+        public int Price { get; set; }
+        public int Pages { get; set; }
     }
 
   
