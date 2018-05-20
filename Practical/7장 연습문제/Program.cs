3eebd98d8defc7caca1218a84b84f90f73ec2fdd
mycubicle13 <mycubicle13@gmail.com>
2018-05-20 09:28:31 +0900

    7.1.2

--- Practical/7장 연습문제/Program.cs
+++ Practical/7장 연습문제/Program.cs
@@ -10,27 +10,46 @@ namespace _7장_연습문제
     {
         static void Main(string[] args)
         {
-            var line = "Cozy lummox gives smart squid who asks for job pen" ;
+            var line = "Cozy lummox gives smart squid who asks for job pen";
 
-            var alphabetcount = new Dictionary<char, int>();
+            /*           var alphabetcount = new Dictionary<char, int>();
+
+                       foreach (var item in line)
+                       {
+                           var temp = char.ToUpper(item);
+                           if ('A' <= temp && temp <= 'Z')
+                           {
+                               if (alphabetcount.ContainsKey(temp))
+                                   alphabetcount[temp]++;
+                               else
+                                   alphabetcount[temp] = 1;
+                           }
+                       }
+
+                       foreach (var item in alphabetcount.OrderBy(x => x.Key)) 
+                       {
+                           Console.WriteLine($"{item.Key} : {item.Value}");
+                       }
+           */
+
+            var neworder = new SortedDictionary<char, int>();
 
             foreach (var item in line)
             {
                 var temp = char.ToUpper(item);
                 if ('A' <= temp && temp <= 'Z')
                 {
-                    if (alphabetcount.ContainsKey(temp))
-                        alphabetcount[temp]++;
+                    if (neworder.ContainsKey(temp))
+                        neworder[temp]++;
                     else
-                        alphabetcount[temp] = 1;
+                        neworder[temp] = 1;
                 }
             }
 
-            foreach (var item in alphabetcount.OrderBy(x => x.Key)) 
+            foreach (var item in neworder)
             {
                 Console.WriteLine($"{item.Key} : {item.Value}");
             }
-
-        }  
+        }
     }
 }
