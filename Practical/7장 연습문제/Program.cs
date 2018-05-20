9f76b96dee68495c750f54e540439b546d0cb821
mycubicle13 <mycubicle13@gmail.com>
2018-05-20 09:21:38 +0900

    연습문제 7.1.1

---
+++ Practical/7장 연습문제/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7장_연습문제
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var line = "Cozy lummox gives smart squid who asks for job pen" ;
+
+            var alphabetcount = new Dictionary<char, int>();
+
+            foreach (var item in line)
+            {
+                var temp = char.ToUpper(item);
+                if ('A' <= temp && temp <= 'Z')
+                {
+                    if (alphabetcount.ContainsKey(temp))
+                        alphabetcount[temp]++;
+                    else
+                        alphabetcount[temp] = 1;
+                }
+            }
+
+            foreach (var item in alphabetcount.OrderBy(x => x.Key)) 
+            {
+                Console.WriteLine($"{item.Key} : {item.Value}");
+            }
+
+        }  
+    }
+}
