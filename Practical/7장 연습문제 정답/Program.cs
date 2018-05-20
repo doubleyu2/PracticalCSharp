dd8a6ea9cd2f5581c43ec79df915a2ea17ed1711
mycubicle13 <mycubicle13@gmail.com>
2018-05-20 10:52:56 +0900

    연습문제 7.1.1

---
+++ Practical/7장 연습문제 정답/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7장_연습문제_정답
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var line = "Cozy lummox gives smart squid who asks for job pen";
+            var dict = new Dictionary<char, int>();
+
+            foreach (var item in line)
+            {
+                var ch = char.ToUpper(item);
+
+                if ('A' <= ch && ch <= 'Z')
+                {
+                    if (dict.ContainsKey(ch))
+                    {
+                        dict[ch]++;
+                    }
+                    //else (!dict.ContainsKey(ch))
+                    else 
+                    {
+                        dict[ch] = 1;
+                    }
+                } 
+            }
+
+            foreach (var item in dict.OrderBy(p => p.Key))
+            {
+                Console.WriteLine($"{item.Key}:{item.Value}");
+            }
+        }
+    }
+}
