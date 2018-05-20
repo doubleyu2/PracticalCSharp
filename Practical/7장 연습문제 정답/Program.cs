11bf3a3ecd24f287a36ae877e2cf61c5d100cf33
mycubicle13 <mycubicle13@gmail.com>
2018-05-20 11:02:42 +0900

    7.2.1 Count

--- Practical/7장 연습문제 정답/Program.cs
+++ Practical/7장 연습문제 정답/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,31 +11,71 @@ namespace _7장_연습문제_정답
     {
         static void Main(string[] args)
         {
-            var line = "Cozy lummox gives smart squid who asks for job pen";
-            var dict = new SortedDictionary<char, int>();
+            // 생성자를 호출한다
+            var abbrs = new Abbreviations();
 
-            foreach (var item in line)
+            // Add 메서드를 호출한 예
+            abbrs.Add("IOC", "국제 올림픽 위원회");
+            abbrs.Add("NPT", "핵확산방지조약");
+
+            // 7.2.1 Count
+            Console.WriteLine();
+            var count = abbrs.Count;
+            Console.WriteLine(count);
+
+
+
+        }
+    }
+
+
+    class Abbreviations
+    {
+        private Dictionary<string, string> _dict = new Dictionary<string, string>();
+
+        // 생성자
+        public Abbreviations()
+        {
+            var lines = File.ReadAllLines("Abbreviations.txt");
+            _dict = lines.Select(line => line.Split('='))
+                         .ToDictionary(x => x[0], x => x[1]);
+        }
+
+        // 요소를 추가한다
+        public void Add(string abbr, string japanese)
+        {
+            _dict[abbr] = japanese;
+        }
+
+        // 인덱서: 줄임말을 키로 사용한다
+        public string this[string abbr]
+        {
+            get
             {
-                var ch = char.ToUpper(item);
-
-                if ('A' <= ch && ch <= 'Z')
-                {
-                    if (dict.ContainsKey(ch))
-                    {
-                        dict[ch]++;
-                    }
-                    //else (!dict.ContainsKey(ch))
-                    else 
-                    {
-                        dict[ch] = 1;
-                    }
-                } 
-            }
-
-            foreach (var item in dict)
-            {
-                Console.WriteLine($"{item.Key}:{item.Value}");
+                return _dict.ContainsKey(abbr) ? _dict[abbr] : null;
             }
         }
+
+        // 한국어로부터 그에 대응되는 줄임말을 구한다
+        public string ToAbbreviation(string japanese)
+        {
+            return _dict.FirstOrDefault(x => x.Value == japanese).Key;
+        }
+
+        // 한국어의 위치를 인수에 넘겨주고 그것이 포함되는 요소(Key,Value)를 모두 구한다
+        public IEnumerable<KeyValuePair<string, string>> FindAll(string substring)
+        {
+            foreach (var item in _dict)
+            {
+                if (item.Value.Contains(substring))
+                    yield return item;
+            }
+        }
+
+        public int Count        
+        {
+            get { return _dict.Count; }
+        }
+
     }
 }
