2a1e8ec517d332096e2b86eff9e5311d7ef9efc0
mycubicle13 <mycubicle13@gmail.com>
2018-05-20 12:06:07 +0900

    AddSeconds

--- Practical/8장/Program.cs
+++ Practical/8장/Program.cs
@@ -11,13 +11,37 @@ namespace _8장
         static void Main(string[] args)
         {
 
-            var date = DateTime.Now;
+            var dt1 = new DateTime(2006, 10, 18, 1, 30, 21);
+            var dt2 = new DateTime(2006, 11, 2, 18, 5, 28);
+            if (dt1 < dt2)
+                Console.WriteLine("dt2 이 미래");
+            else if (dt1 == dt2)
+                Console.WriteLine("같은 시각입니다");
 
-            string s1 = date.ToString("yyyy-MM-dd");
-            int m = date.Month;
 
-            Console.WriteLine(s1);
-            
+            var diff = dt2 - dt1;
+
+            double total = diff.TotalSeconds;
+
+            Console.WriteLine(total);
+
+
+
+            var today = DateTime.Now;
+
+            var future = today.AddSeconds(5);
+
+            while(true)
+            {
+                today = DateTime.Now;
+                var fd = future - today;
+
+                if (fd.TotalSeconds <= 0)
+                {
+                    Console.WriteLine("끝");
+                    break;
+                }
+            }
         }
     }
 }
