b6c87fd5ce99fc97f1c0d2e939d114cfdd07b3c9
mycubicle13 <mycubicle13@gmail.com>
2018-05-22 10:34:19 +0900

    9.1.1 using

---
+++ Practical/9장/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9장
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var filePath = @"c:\Example\Greeting.txt";
+
+            if (File.Exists(filePath))
+            {
+                using (var reader = new StreamReader(filePath, Encoding.UTF8))
+                {
+                    while(!reader.EndOfStream)
+                    {
+                        var line = reader.ReadLine();
+                        Console.WriteLine(line);
+                    }
+                }
+               
+            }
+        }
+    }
+}
