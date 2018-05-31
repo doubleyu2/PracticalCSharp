0e832799137dd603f107abe13e42d7dc9b79fb87
mycubicle13 <mycubicle13@gmail.com>
2018-05-31 09:51:40 +0900

    10.1 표10.1 정규식 특수기호

---
+++ Practical/10장/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _10장
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Regex reg = new Regex(@"(010)[- ]?(\d{4})[- ]?(\d{4})");
+
+            string text = "안녕하세요 01037397593";
+
+            Match match = reg.Match(text);
+
+            if (match.Success)
+            {
+                string replace = reg.Replace(match.Value, "$1-$2-$3");
+
+                Console.WriteLine(replace);
+            }
+
+
+
+        }
+    }
+}
