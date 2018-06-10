2377eb2aadcfe684bcd9a55b9c8be3eade3246a1
mycubicle13 <mycubicle13@gmail.com>
2018-06-10 11:56:39 +0900

    11.3.4 Collection 으로 생성

--- Practical/11장/Program.cs
+++ Practical/11장/Program.cs
@@ -7,47 +7,59 @@ using System.Xml.Linq;
 
 namespace _11장
 {
+    
+    public class Novelist
+    {
+        public string Name { get; set; }
+        public string EngName { get; set; }
+        public DateTime Birth { get; set; }
+        public DateTime Death { get; set; }
+        public IEnumerable<string> Masterpieces { get; set; }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-
-
-            var novelists = new XElement("novelists",
-                new XElement("novelist",
-                    new XElement("name", "마크 트웨인", new XAttribute("eng", "Mark Twain")),
-                    new XElement("birth", "1835-11-30"),
-                    new XElement("death", "1910-03-21")
-                   ),
-                new XElement("novelist",
-                    new XElement("name", "어니스트 헤밍웨이")));
-
-                   
-
-            string xmlstring =
-                  @"<?xml version=""1.0"" encoding=""utf-8"" ?>
-                    <novelists>
-                      <novelist>
-                        <name eng=""Agatha Christie"">아가사 크리스티</name>
-                        <birth>1890-09-15</birth>
-                        <death>1976-01-12</death>
-                        <masterpieces>
-                          <title>그리고 아무도 없었다</title>
-                          <title>오리엔트 특급 살인</title>
-                        </masterpieces>
-                      </novelist>
-                    </novelists>";
-
-            var xdoc = XDocument.Parse(xmlstring);
-
-            foreach (var item in xdoc.Root.Elements())
+            var novelists = new List<Novelist>
             {
-                var birthD = item.Element("birth");
-                var deathD = item.Element("death");
-                Console.WriteLine($"{birthD.Value},  ,{deathD.Value}");
-            }
+                new Novelist
+                {
+                    Name = "마크 트웨인",
+                    EngName = "Mark Twain",
+                    Birth = DateTime.Parse("1835-11-30"),
+                    Death = DateTime.Parse("1910-03-21"),
+                    Masterpieces = new string[]{"톰 소여의 모험","허클베리 핀의 모험"},
 
-        }
+                },
+
+                new Novelist
+                {
+                    Name = "어니스트 헤밍웨이",
+                    EngName = "Ernest Hemingway",
+                    Birth = DateTime.Parse("1899-07-21"),
+                    Death = DateTime.Parse("1961-07-02"),
+                    Masterpieces = new string[]{"무기여 잘있거라","노인과 바다",},
+                },
+            };
+
+            var elements = novelists.Select(x =>
+                                            new XElement("novelist",
+                                                new XElement("name", x.Name, new XAttribute("eng", x.EngName)),
+                                                new XElement("birth", x.Birth),
+                                                new XElement("death", x.Death),
+                                                new XElement("masterpieces", x.Masterpieces.Select(t => new XElement("title", t)))
+                                            ));
+            var root = new XElement("novelists", elements);
+            var xdoc = new XDocument(root);
+
+            foreach (var element in xdoc.Root.Elements())
+            {
+                Console.WriteLine(element.Element("name").Value);
+            }
+        }       
 
     }
+
+   
 }
