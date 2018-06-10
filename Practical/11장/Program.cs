c38a98442f87caf2e88754970fbbf073fee175a3
mycubicle13 <mycubicle13@gmail.com>
2018-06-10 10:21:57 +0900

    11.2.1 XElements

---
+++ Practical/11장/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace _11장
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var xdoc = XDocument.Load("novelists.xml");
+            var xelements = xdoc.Root.Elements();
+            foreach (var xnovelist in xelements)
+            {
+                XElement xname = xnovelist.Element("name");
+                Console.WriteLine(xname.Value);
+            }
+        }
+    }
+}
