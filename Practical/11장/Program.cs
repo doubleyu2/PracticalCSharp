8a9b6a8ba15dea84ce0f9edd601497b86ec28683
mycubicle13 <mycubicle13@gmail.com>
2018-06-10 11:26:34 +0900

    11.2.9 function, class Novelist

--- Practical/11장/Program.cs
+++ Practical/11장/Program.cs
@@ -11,19 +11,47 @@ namespace _11장
     {
         static void Main(string[] args)
         {
-            var xdoc = XDocument.Load("novelists.xml");
-            var novelists = xdoc.Root.Elements()
-                                     .Select(x => new
-                                     {
-                                         Name = (string)x.Element("name"),
-                                         Birth = (DateTime)x.Element("birth"),
-                                         Death = (DateTime)x.Element("death"),
-                                     });
+
+            var novelists = ReadNovelists();
 
             foreach (var novelist in novelists)
             {
-                Console.WriteLine($"Name: {novelist.Name}, Birth: {novelist.Birth.Year}, Death: {novelist.Death.Year}");
-            }
+                Console.WriteLine($"Name: {novelist.Name}, Birth: {novelist.Birth.Year}, Death: {novelist.Death.Year}, {string.Join("-, ", novelist.Masterpieces)}");
+            }                
+        
         }
+
+        static IEnumerable<Novelist> ReadNovelists()
+        {
+            var xdoc = XDocument.Load("novelists.xml");
+            var novelists = xdoc.Root.Elements()
+                                     .Select(x => new Novelist
+                                     {
+                                         Name = (string)x.Element("name"),
+                                         EngName = (string)(x.Element("name").Attribute("eng")),
+                                         Birth = (DateTime)x.Element("birth"),
+                                         Death = (DateTime)x.Element("death"),
+                                         Masterpieces = x.Element("masterpieces")
+                                                        .Elements("title")
+                                                        .Select(title => title.Value)
+                                                        .ToArray()
+                                     });
+            return novelists.ToArray();
+        }
+
     }
+
+
+    class Novelist
+    {
+        public string Name { get; set; }
+        public string EngName { get; set; }
+        public DateTime Birth { get; set; }
+        public DateTime Death { get; set; }
+        public IEnumerable<string> Masterpieces { get; set; }
+
+
+    }
+
+  
 }
