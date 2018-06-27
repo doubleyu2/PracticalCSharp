7b93b16448da927ddbaf245b2f83f46966c3c7ff
mycubicle13 <mycubicle13@gmail.com>
2018-06-27 10:35:41 +0900

    12.1.1 XML 파일 만들기

---
+++ Practical/12장/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using System.Xml;
+
+namespace _12장
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var novel = new Novel
+            {
+                Author = "제임스 P. 호건",
+                Title = "별의 계승자",
+                Published = 1997,
+            };
+
+            var settings = new XmlWriterSettings
+            {
+                Encoding = new System.Text.UTF8Encoding(false),
+                Indent = true,
+                IndentChars = " ",
+            };
+
+            using (var writer = XmlWriter.Create("novel.xml", settings))
+            {
+                var serializer = new DataContractSerializer(novel.GetType());
+                serializer.WriteObject(writer, novel);
+            }
+        }
+    }
+
+    public class Novel
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int Published { get; set; }
+        public override string ToString()
+        {
+            return string.Format($"Title: {Title}, Author: {Author}, Published: {Published}");
+        }
+    }
+}
