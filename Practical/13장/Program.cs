478ff5aecddea5b3321aecc17d6248ce159bf846
mycubicle13 <mycubicle13@gmail.com>
2018-07-15 09:45:12 +0900

    13.10 수준높은 쿼리

--- Practical/13장/Program.cs
+++ Practical/13장/Program.cs
@@ -3,29 +3,52 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using 13장.Models;
+using _13장.Models;
 
-namespace 13장
+namespace _13장
 {
     class Program
     {
         static void Main(string[] args)
         {
+            /*using (var db = new BooksDbContext())
+            {
+                var book = db.Books.Single(x => x.Title == "별의 계승자");
+                book.PublishedYear = 2016;
+                db.SaveChanges();
+            }*/
+
+            var db = new BooksDbContext();
 
             var authors = db.Authors.Where(a => a.Books.Count() >= 2);
-            foreach (var author in authors)
+
+            foreach (var item in authors)
             {
-                Console.WriteLine($"{author.Name} {author.Gender} {author.Birthday}");
+                Console.WriteLine($"{item.Name} {item.Gender} {item.Birthday}");
             }
 
-            var books = DBNull.Books.Orderby(b => b.PublishedYear).ThenBy(b => b.Author.Name);
+            var books = db.Books.OrderBy(b => b.PublishedYear).ThenBy(b => b.Author.Name);
             foreach (var book in books)
             {
                 Console.WriteLine($"{book.Title} {book.PublishedYear} {book.Author.Name}");
             }
 
-            var groups = DBNull.
+            var groups = db.Books.GroupBy(b => b.PublishedYear).Select(g => new
+            {
+                Year = g.Key,
+                Count = g.Count(),
+            });
+
+            foreach (var g in groups)
+            {
+                Console.WriteLine($"{g.Year} {g.Count}");
+            }
+
+            var author = db.Authors.Where(a => a.Books.Count() == db.Authors.Max(x => x.Books.Count())).First();
+
+            Console.WriteLine($"{author.Name} {author.Gender} {author.Birthday}");
         }
+
 
   
     }
